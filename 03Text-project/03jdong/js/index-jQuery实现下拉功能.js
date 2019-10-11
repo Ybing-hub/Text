d104193230fd0164bca3a20762172737cabb7a6e
Gityaobing <YaoBingvip@163.com>
2019-10-11 13:50:19 +0800

    add 10-11

--- 03Text-project/03jdong/js/index-jQuery实现下拉功能.js
+++ 03Text-project/03jdong/js/index-jQuery实现下拉功能.js
@@ -2,11 +2,23 @@
 	var $dropdown = $('.nav .dropdown')
 	$($dropdown).dropdown({delay:200})
 	$($dropdown).on('dropdown-show',function(ev){
-		var dataUrl = $dropdown.data('url');
-		if ($dropdown.data('url')) {	
-			$.getJSON('',function(data){
-			})
-		}
+		var $elem = $(this)
+		var dataUrl = $elem.data('url');
+		console.log(dataUrl)
+		if(!dataUrl) return
+		if($elem.data('isLoaded')) return
+		$.getJSON(dataUrl,function(data){
+			$elem.data('isLoaded',true)
+			var html = '';
+			for(var i = 0;i<data.length;i++){
+				html+= '<li class="menu-item"><a href="'+data[i].url+'">'+data[i].name	+'</a></li>'
+			}
+
+			setTimeout(function(){
+				$dropdown.find('.dropdown-layer').html(html)
+			},1000)
+		})
+		
 		
 	})
 
